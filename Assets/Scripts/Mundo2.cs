0ba049082546c736eaa2eef52f6b35c3ce2f9772
thesolidpakman <45268177+thesolidpakman@users.noreply.github.com>
2019-02-19 17:52:02 +0100

    Desaparece

---
+++ Assets/Scripts/Mundo2.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mundo2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+
+
+    public void Desaparece()
+    {
+        gameObject.SetActive(false);
+    }
+
+
+
+
+}
