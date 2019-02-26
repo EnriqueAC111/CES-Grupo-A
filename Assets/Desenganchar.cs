1cd3fb2ba47b166ab830c8bd17ecc39708398520
EnriqueAC111 <enrique.anguis@gmail.com>
2019-02-26 18:21:03 +0100

    Revert "Revert "#111 Montar niveles (Lvl1)""
    This reverts commit 150e8850cf8a61d6e4b4513b3ce8b34ddfe625d8.

---
+++ Assets/Desenganchar.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Desenganchar : MonoBehaviour
+{
+
+	HingeJoint2D hj;
+    // Start is called before the first frame update
+    void Start()
+    {
+		hj = GetComponent<HingeJoint2D> ();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if(other.gameObject.CompareTag("Kaze"))
+			{
+			hj.enabled = false;
+			}
+	}
+}
--- Assets/Scripts/Particulas.cs
+++ Assets/Scripts/Particulas.cs
@@ -34,5 +34,6 @@ public class Particulas : MonoBehaviour
 		{
 			particulas.SetActive(true);
 		}
+			
 	}
 }
