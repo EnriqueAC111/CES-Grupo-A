8b9992bbfde208f7af725533989071db9a247d53
EnriqueAC111 <enrique.anguis@gmail.com>
2019-03-06 18:16:25 +0100

    Ventilador fixed #111 Montar niveles: acabada #119 Pulir: en proceso

--- Assets/Scripts/Ventilador.cs
+++ Assets/Scripts/Ventilador.cs
@@ -7,7 +7,7 @@ public class Ventilador : MonoBehaviour
 
     PointEffector2D pe;
     Animator anim;
-    private Vector2 mousePosition;
+    
 
 
     public EfectosSonido efs;
@@ -21,31 +21,19 @@ public class Ventilador : MonoBehaviour
     }
 
     // Update is called once per frame
-     /*  void Update()
-    {
-        mousePosition = Input.mousePosition;
-        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
-        if((Input.GetMouseButton(0)) && (mousePosition = transform.position))
-        {
-        Debug.Log("tocado");
-        pe.enabled = true;
-        anim.SetBool("Activado", true);
-        }
-        if(Input.GetMouseButtonUp(0))
-        {
-        pe.enabled = false;
-        anim.SetBool("Activado", false);
-        }
 
-
-    }
-*/
     private void OnMouseDown()
     {
         Debug.Log("tocado");
         pe.enabled = true;
         anim.SetBool("Activado", true);
         efs.SonidoVentilador();
+    }
+    private void OnMouseUp()
+    {
+        Debug.Log("tocado");
+        pe.enabled = false;
+        anim.SetBool("Activado", false);
     }
 
 
