7d8fa1eed02499add77c835e0224e7605c094aa4
EnriqueAC111 <enrique.anguis@gmail.com>
2019-03-06 17:43:29 +0100

    Lvl 3 funciona

--- Assets/Scripts/Ventilador.cs
+++ Assets/Scripts/Ventilador.cs
@@ -7,6 +7,7 @@ public class Ventilador : MonoBehaviour
 
     PointEffector2D pe;
     Animator anim;
+    private Vector2 mousePosition;
     
 
     // Start is called before the first frame update
@@ -18,16 +19,25 @@ public class Ventilador : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+     /*  void Update()
     {
-        if(Input.GetMouseButtonDown(0))
+        mousePosition = Input.mousePosition;
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        if((Input.GetMouseButton(0)) && (mousePosition = transform.position))
         {
         Debug.Log("tocado");
         pe.enabled = true;
         anim.SetBool("Activado", true);
         }
-    }
+        if(Input.GetMouseButtonUp(0))
+        {
+        pe.enabled = false;
+        anim.SetBool("Activado", false);
+        }
 
+
+    }
+*/
     private void OnMouseDown()
     {
         Debug.Log("tocado");
